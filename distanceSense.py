f84cc6b7d63f05f4ed73bcc12e1ba70e5430a657
Heather Dykstra <missheatherlaree@gmail.com>
2019-05-16 12:36:06 -0600

    starting to add in Salesforce logic

--- distanceSense.py
+++ distanceSense.py
@@ -1,9 +1,27 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
+import salesforce_requests_oauthlib import SalesforceOAuth2Session
+import salesforce_streaming_client import SalesforceStreamingClient
+import salesforce_streaming_client import _decode_set
+import salesforce_streaming_client import _encode_set
 
+import json
+
+#Declare GPIO Setup
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+#Declare Salesforce Setup
+salesforce_client_id = ''
+salesforce_client_secret = ''
+username = ''
+password = ''
+
+EVENT_TRAIN_STOP = 'Train_Stop'
+
+
+
 
 try:
     while True:
